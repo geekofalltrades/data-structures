78369298b12041018cf574d3de581756182dd362
geekofalltrades <thegeekofalltrades@gmail.com>
2014-02-18 18:55:56 -0800

    Began adding LinkedList functionality. Is it to spec? I won't know until I get home and have internet and can actually see the spec.

--- linked_list.py
+++ linked_list.py
@@ -3,9 +3,31 @@ class LinkedList(object):
         self.head = None
 
     def __str__(self):
-        pass
+        retval = []
+        nextnode = self.head
+        while nextnode != None:
+            retval.append(nextnode.value)
+            nextnode = nextnode.next
+
+        return retval
 
     def count(self):
+        ct = 0
+        nextnode = self.head
+        while nextnode != None:
+            nextnode = nextnode.next
+            ct += 1
+
+        return ct
+
+    def find(self, findval):
+        nextnode = self.head
+        while nextnode != None:
+            if nextnode.value == findval:
+                return True
+        return False
+
+    def remove(self, item):
         pass
 
 
@@ -13,3 +35,8 @@ class LinkedListNode(object):
     def __init__(self, value=None):
         self.value = value
         self.next = None
+
+
+if __name__ == '__main__':
+    pass
+    #eventually run some demonstration code here?
