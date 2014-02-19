cd46a18e9cabe5ac7a9d144578a55081fd715497
geekofalltrades <thegeekofalltrades@gmail.com>
2014-02-19 10:20:33 -0800

    Completely fleshed out unit test skeleton file with all tests that should be implemented.

--- test_linked_list.py
+++ test_linked_list.py
@@ -3,6 +3,7 @@ import unittest
 
 
 class TestInsertFunction(unittest.TestCase):
+    """Test the LinkedList's insert method."""
     def setUp(self):
         self.insert_string = 'bob'
         self.insert_int = 14
@@ -85,23 +86,77 @@ class TestInsertFunction(unittest.TestCase):
 
 
 class TestPopFunction(unittest.TestCase):
-    pass
+    """Test the LinkedList's pop method."""
+    def setUp(self):
+        pass
 
+    def test_pop_from_empty_list(self):
+        """Pop from a LinkedList that is empty."""
+        pass
+
+    def test_pop_from_one_list(self):
+        """Pop from a LinkedList with one node."""
+        pass
+
+    def test_pop_from_many_list(self):
+        """Pop from a LinkedList with many nodes."""
+        pass
 
 class TestSizeFunction(unittest.TestCase):
-    pass
+    """Test the LinkedList's size method."""
+    def setUp(self):
+        pass
+
+    def test_size_of_empty_list(self):
+        """Get the size of an empty LinkedList."""
+
+    def test_size_of_populated_list(self):
+        """Get the size of a populated LinkedList."""
 
 
 class TestSearchFunction(unittest.TestCase):
-    pass
+    """Test the LinkedList's search method."""
+    def setUp(self):
+        pass
+
+    def test_search_empty_list(self):
+        """Search for a value in an empty LinkedList."""
+
+    def test_search_list_without_value(self):
+        """Search a populated LinkedList for a value it doesn't contain."""
+
+    def test_serach_list_with_value(self):
+        """Search a populated LinkedList for a value it contains."""
 
 
 class TestRemoveFunction(unittest.TestCase):
-    pass
+    """Test the LinkedList's remove method."""
+    def setUp(self):
+        pass
+
+    def test_remove_empty_list(self):
+        """Remove from an empty LinkedList."""
+
+    def test_remove_list_without_value(self):
+        """Remove from a populated LinkedList a value it doesn't contain."""
+
+    def test_remove_list_with_value(self):
+        """Remove from a populated LinkedList a value it contains."""
 
 
 class TestStrFunction(unittest.TestCase):
-    pass
+    """Test the LinkedList's ___str___ method."""
+    def setUp(self):
+        pass
+
+    def test_str_empty_list():
+        """Get the string representation of an empty LinkedList."""
+
+    def test_str_one_list():
+        """Get the string representation of a LinkedList with one value."""
+
+    def test_str_many_list():
+        """Get the string representation of a LinkedList with many values."""
 
 
 if __name__ == '__main__':
