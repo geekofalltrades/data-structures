2de106e240f2977d9140f0923d5253f635c347f3
geekofalltrades <thegeekofalltrades@gmail.com>
2014-02-18 18:47:10 -0800

    Initial linked_list commit: added README and linked_list.py with skeleton LinkedList and LinkedListNode classes.

---
+++ README.md
@@ -0,0 +1,7 @@
+Data Structures
+
+These are various implementations of classic data structures in Python.
+
+linked_list.py
+A singly-linked list. Implements the LinkedList class, and can be
+imported or run directly for a demonstration.
---
+++ linked_list.py
@@ -0,0 +1,15 @@
+class LinkedList(object):
+    def __init__(self):
+        self.head = None
+
+    def __str__(self):
+        pass
+
+    def count(self):
+        pass
+
+
+class LinkedListNode(object):
+    def __init__(self, value=None):
+        self.value = value
+        self.next = None
