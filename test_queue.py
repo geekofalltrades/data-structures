91b1c0520fcaf53406247ad9b9d696be53ff6e62
geekofalltrades <thegeekofalltrades@gmail.com>
2014-02-24 16:45:22 -0800

    Added tests for size function of queue.

--- test_queue.py
+++ test_queue.py
@@ -64,5 +64,44 @@ class TestDequeue(unittest.TestCase):
         self.assertTrue(q.head is None)
         self.assertTrue(q.tail is None)
 
+
+class TestSize(unittest.TestCase):
+    """Test the size method of the queue."""
+
+    def setUp(self):
+        self.values = ['bob', 'fred', range(3), 3.14]
+        self.single_value = 'teststring'
+
+    def test_size_of_empty_queue(self):
+        """Take the size of an empty queue."""
+        q = Queue()
+        self.assertTrue(q.head is None)
+        self.assertTrue(q.tail is None)
+        self.assertEquals(q.size(), 0)
+
+    def test_size_of_populated_queue(self):
+        """Take the size of a populated queue."""
+        q = Queue()
+        for val in self.values:
+            q.queue(val)
+        self.assertEqual(q.size(), len(self.values))
+
+    def test_size_of_depopulated_queue(self):
+        """Take the size of a queue that has been populated and then had
+        some items dequeued."""
+        q = Queue()
+        for val in self.values:
+            q.queue(val)
+        q.dequeue()
+        self.assertEqual(q.size(), len(self.values) - 1)
+
+    def test_size_of_empty_queue_popped_from(self):
+        """Take the size of a queue that has been dequeued from its empty
+        state."""
+        q = Queue()
+        self.assertRaises(EmptyError, q.dequeue)
+        self.assertEqual(q.size(), 0)
+
+
 if __name__ == '__main__':
     unittest.main()
