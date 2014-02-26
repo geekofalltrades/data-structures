b190bb2790fefd4e632f3ac2ada0b965b95fae33
geekofalltrades <thegeekofalltrades@gmail.com>
2014-02-25 19:54:00 -0800

    Added hash table and its tests. Skeleton files.

---
+++ hash_table.py
@@ -0,0 +1,20 @@
+from linked_list import LinkedList
+
+
+class HashTable(object):
+    """A very naive hast table. Hashes keys (only strings allowed) by the
+    summed ordinal value of their characters, and prevents collisions by
+    binning any hashed values that collide.
+    """
+
+    def __init__(self):
+        pass
+
+    def get(self):
+        pass
+
+    def set(self):
+        pass
+
+    def hash(self):
+        pass
---
+++ test_hash_table.py
@@ -0,0 +1,18 @@
+import unittest
+from hash_table import HashTable
+
+
+class TestGet(unittest.TestCase):
+    pass
+
+
+class TestSet(unittest.TestCase):
+    pass
+
+
+class TestHash(unittest.TestCase):
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
