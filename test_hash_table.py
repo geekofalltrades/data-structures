8a9cec5107fbd7f4942cd8d087c828799f65c726
geekofalltrades <thegeekofalltrades@gmail.com>
2014-02-25 20:50:26 -0800

    Implemented tests for set and hash functions of hash table.

--- test_hash_table.py
+++ test_hash_table.py
@@ -43,7 +43,6 @@ class TestSet(unittest.TestCase):
     """
 
     def setUp(self):
-        self.pairs = {'aab': 14, 'aVal': 'blargh', 'nutherVal': 3.14}
         self.colliding_pair = ('aba', 'iCollide')
         self.present_pair = ('aab', 14)
         self.overwrite_pair = ('aab', 17)
@@ -51,14 +50,26 @@ class TestSet(unittest.TestCase):
     def test_set_on_empty_slot(self):
         """Set a value using a key that hashes to an empty bin."""
         h = HashTable()
+        h.set(self.present_pair[0], self.present_pair[1])
+        self.assertEqual(self.present_pair[1], h.get(self.present_pair[0]))
 
     def test_set_on_slot_with_collisions(self):
         """Set a value using a key that hashes to a bin already containing
         other keys.
         """
+        h = HashTable()
+        h.set(self.present_pair[0], self.present_pair[1])
+        h.set(self.colliding_pair[0], self.colliding_pair[1])
+        self.assertEqual(self.present_pair[1], h.get(self.present_pair[0]))
+        self.assertEqual(self.colliding_pair[1], h.get(self.colliding_pair[0]))
 
     def test_set_on_existing_key(self):
         """Set a value using a key that already exists."""
+        h = HashTable()
+        h.set(self.present_pair[0], self.present_pair[1])
+        self.assertEqual(self.present_pair[1], h.get(self.present_pair[0]))
+        h.set(self.overwrite_pair[0], self.overwrite_pair[1])
+        self.assertEqual(self.overwrite_pair[1], h.get(self.overwrite_pair[0]))
 
 
 class TestHash(unittest.TestCase):
@@ -67,13 +78,22 @@ class TestHash(unittest.TestCase):
     """
 
     def setUp(self):
-        pass
+        self.invalid_key = 14
+        self.valid_key = 'bob'
 
     def test_hash_on_invalid_key(self):
-        pass
+        """Try to hash an invalid key (not a string)."""
+        h = HashTable()
+        self.assertRaises(ValueError, h.hash, self.invalid_key)
 
     def test_hash_on_valid_key(self):
-        pass
+        """Verify that a string key hashes to the expected value. Somewhat
+        tautological.
+        """
+        h = HashTable()
+        hashed = h.hash(self.valid_key)
+        expected = sum([ord(i) for i in self.valid_key.split('')]) % 16
+        self.assertEqual(hashed, expected)
 
 
 if __name__ == '__main__':
