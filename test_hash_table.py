7169b1e7e4f48325850a089f92a6c58fc15c4a9f
geekofalltrades <thegeekofalltrades@gmail.com>
2014-02-25 20:02:02 -0800

    Fleshed out hash test skeleton with function definitions for each test.

--- test_hash_table.py
+++ test_hash_table.py
@@ -3,15 +3,54 @@ from hash_table import HashTable
 
 
 class TestGet(unittest.TestCase):
-    pass
+    """Test the get function of the hash (retrieves values based on their
+    key).
+    """
+
+    def setUp(self):
+        pass
+
+    def test_get_from_empty_hash(self):
+        pass
+
+    def test_get_from_hash_containing_value(self):
+        pass
+
+    def test_get_from_hash_not_containing_value(self):
+        pass
+
+    def test_get_from_hash_with_collisions(self):
+        pass
 
 
 class TestSet(unittest.TestCase):
-    pass
+    """Test the set function of the hash (stores a value at the location
+    determined by its hashed key).
+    """
+
+    def setUp(self):
+        pass
+
+    def test_set_on_empty_slot(self):
+        pass
+
+    def test_set_on_slot_with_collisions(self):
+        pass
 
 
 class TestHash(unittest.TestCase):
-    pass
+    """Test the hash function of the hash (transform a key into a location
+    in the hash's storage).
+    """
+
+    def setUp(self):
+        pass
+
+    def test_hash_on_invalid_key(self):
+        pass
+
+    def test_hash_on_valid_key(self):
+        pass
 
 
 if __name__ == '__main__':
