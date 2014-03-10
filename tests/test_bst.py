bada333f40da5400f4c8d1dd15ed83ffb410dff7
geekofalltrades <thegeekofalltrades@gmail.com>
2014-03-10 12:53:56 -0700

    Added binary search tree file and corresponding tests file. Wrote out skeleton unit tests.

---
+++ tests/test_bst.py
@@ -0,0 +1,83 @@
+import unittest
+
+
+class TestInsert(unittest.TestCase):
+    """Test the insert method of the binary search tree class."""
+    def setUp(self):
+        pass
+
+    def test_insert_into_empty_tree(self):
+        pass
+
+    def test_insert_to_left(self):
+        pass
+
+    def test_insert_to_right(self):
+        pass
+
+
+class TestContains(unittest.TestCase):
+    """Test the contains method of the binary search tree class."""
+    def setUp(self):
+        pass
+
+    def test_contains_on_empty_tree(self):
+        pass
+
+    def test_contains_on_head_node(self):
+        pass
+
+    def test_contains_on_lower_node(self):
+        pass
+
+    def test_contains_on_heavily_populated_tree(self):
+        pass
+
+
+class TestSize(unittest.TestCase):
+    """Test the size method of the binary search tree class."""
+    def setUp(self):
+        pass
+
+    def test_size_on_empty_tree(self):
+        pass
+
+    def test_size_on_populated_tree(self):
+        pass
+
+
+class TestDepth(unittest.TestCase):
+    """Test the depth method of the binary search tree class."""
+    def setUp(self):
+        pass
+
+    def test_depth_on_empty_tree(self):
+        pass
+
+    def test_depth_on_one_level_tree(self):
+        pass
+
+    def test_depth_on_two_level_tree(self):
+        pass
+
+
+class TestBalance(unittest.TestCase):
+    """Test the balance method of the binary search tree class."""
+    def setUp(self):
+        pass
+
+    def test_balance_on_empty_tree(self):
+        pass
+
+    def test_balance_on_head_node(self):
+        pass
+
+    def test_balance_on_right_heavy_tree(self):
+        pass
+
+    def test_balance_on_left_heavy_tree(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
