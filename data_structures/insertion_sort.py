66a15afe1bce7d6bfd98ac0a61420414eae44a53
geekofalltrades <thegeekofalltrades@gmail.com>
2014-03-17 14:48:41 -0700

    Implemented insertion sort. Test is passing.

--- data_structures/insertion_sort.py
+++ data_structures/insertion_sort.py
@@ -1,3 +1,20 @@
 def insertion_sort(li):
     """Sort a list in-place using the insertion sort algorithm."""
-    pass
+    i = 1
+    while(i < len(li)):
+        if li[i] < li[i - 1]:
+            j = 0
+            while(j < i):
+                if li[i] <= li[j]:
+                    break
+                j += 1
+
+            li.insert(j, li.pop(i))
+        i += 1
+
+
+if __name__ == '__main__':
+    li = [4, 3, 2, 1]
+    print li
+    insertion_sort(li)
+    print li
