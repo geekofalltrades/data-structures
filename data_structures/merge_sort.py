8e499b886b151881b26e72716f020a4ae0e200f7
geekofalltrades <thegeekofalltrades@gmail.com>
2014-03-18 16:30:41 -0700

    Added explanatory if __name__ == '__main__' block to the merge sort.

--- data_structures/merge_sort.py
+++ data_structures/merge_sort.py
@@ -1,3 +1,7 @@
+from time import time
+from random import randrange
+
+
 def merge_sort(li):
     """Sort a list using the merge sort algorithm."""
     if len(li) < 2:
@@ -18,4 +22,56 @@ def merge_sort(li):
 
 
 if __name__ == '__main__':
-    pass
+    print """
+The pure, unadulterated merge sort algorithm is O(n*log n) in the average
+case and in the worst case. The average case is one in which the list has
+no prior sorting, and so is completely or mostly unordered.
+"""
+
+    avg = 0
+    for i in range(50):
+        li = [randrange(100000) for i in range(100)]
+        start = time()
+        merge_sort(li)
+        end = time()
+        avg += (end - start) / 50
+
+    print """On a completely random list with 100 values:
+%s (average of 50 attempts)""" % avg
+
+    print """
+The worst case is the case in which the list is already sorted, as this
+forces the merge operation to iterate completely over the list that is
+being merged into in order to place each item that is being merged in (as
+the items being merged in will each be greater than the entire contents
+of the list being merged into).
+"""
+
+    avg = 0
+    for i in range(50):
+        li = [i for i in range(100)]
+        start = time()
+        merge_sort(li)
+        end = time()
+        avg += (end - start) / 50
+
+    print """On a list with 100 values in sequential order:
+%s (average of 50 attempts)""" % avg
+
+    print """
+The best case for the merge sort is the case in which the list is in reverse
+order, as each merge of an item into a sublist in the merge operation
+requires only one step - each item being merged in is less than the entire
+contents of the list being merged into.
+"""
+
+    avg = 0
+    for i in range(50):
+        li = [i for i in range(99, -1, -1)]
+        start = time()
+        merge_sort(li)
+        end = time()
+        avg += (end - start) / 50
+
+    print """On a list with 100 values in reverse-sequential order:
+%s (average of 50 attempts)""" % avg
