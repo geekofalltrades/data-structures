3f88c87e4235cee3c7f44432b458abe8bb135240
geekofalltrades <thegeekofalltrades@gmail.com>
2014-03-18 15:47:23 -0700

    Initial implementation of merge sort.

--- data_structures/merge_sort.py
+++ data_structures/merge_sort.py
@@ -1,3 +1,15 @@
 def merge_sort(li):
-    """Sort a list in-place? using the merge sort algorithm."""
-    pass
+    """Sort a list using the merge sort algorithm."""
+    if len(li) == 1:
+        return li
+    else:
+        one = merge_sort(li[: len(li) / 2])
+        two = merge_sort(li[len(li) / 2 + 1:])
+
+        for i in len(two):
+            for j in len(one):
+                if two[i] < one[j]:
+                    one.insert(j, two[i])
+                    break
+
+        return one
