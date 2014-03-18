2d1456370891bc1fb5de44647c8f1ba8a6de3115
geekofalltrades <thegeekofalltrades@gmail.com>
2014-03-18 09:58:40 -0700

    Implemented tests for merge sort. Added merge_sort.py with a skeleton merge sort.

---
+++ data_structures/merge_sort.py
@@ -0,0 +1,3 @@
+def merge_sort(li):
+    """Sort a list in-place? using the merge sort algorithm."""
+    pass
--- tests/test_sorts.py
+++ tests/test_sorts.py
@@ -1,19 +1,43 @@
 from data_structures.insertion_sort import insertion_sort
+from data_structures.merge_sort import merge_sort
 import unittest
 from random import randrange
 
 
-class TestInsertionSort(unittest.TestCase):
-    """Test the insertion sort."""
-    def test_insertion_sort(self):
-        """Sort 1000 randomly generated lists of numbers and assure that
-        their output matches that of Python's built-in sort.
+class TestSorts(unittest.TestCase):
+    """Test the various sorting algorithms against each other."""
+    def test_insertion_sort_against_native(self):
+        """Sort 100 randomly generated lists of numbers with insertion
+        sort and assert that their output matches that of Python's
+        built-in sort.
         """
         for i in range(100):
-            li = [randrange(0, 1000000) for x in range(1000)]
+            li = [randrange(0, 1000000) for x in range(100)]
             expected = sorted(li)
             insertion_sort(li)
             self.assertEqual(expected, li)
+
+    def test_merge_sort_against_native(self):
+        """Sort 100 randomly generated lists of numbers with merge sort
+        and assert that their output matches that of Python's built-in
+        sort.
+        """
+        for i in range(100):
+            li = [randrange(0, 1000000) for x in range(100)]
+            expected = sorted(li)
+            merge_sort(li)
+            self.assertEqual(expected, li)
+
+    def test_insertion_sort_against_merge_sort(self):
+        """Sort 100 randomly generated lists of numbers with insertion
+        sort and with merge sort and assert that their outputs match.
+        """
+        for i in range(100):
+            insertion = [randrange(0, 1000000) for x in range(100)]
+            merge = insertion[:]
+            insertion_sort(insertion)
+            merge_sort(merge)
+            self.assertEqual(insertion, merge)
 
 
 if __name__ == '__main__':
