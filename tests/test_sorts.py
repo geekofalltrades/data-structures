b80e41ab26e7b87dfad8b0d7cc1cc0075df5ef55
geekofalltrades <thegeekofalltrades@gmail.com>
2014-03-19 15:33:33 -0700

    Implemented tests for quicksort.

--- tests/test_sorts.py
+++ tests/test_sorts.py
@@ -1,5 +1,6 @@
 from data_structures.insertion_sort import insertion_sort
 from data_structures.merge_sort import merge_sort
+from data_structures.quicksort import quicksort
 import unittest
 from random import randrange
 
@@ -28,6 +29,17 @@ class TestSorts(unittest.TestCase):
             li = merge_sort(li)
             self.assertEqual(expected, li)
 
+    def test_quicksort_against_native(self):
+        """Sort 100 randomly generated lists of numbers with quicksort
+        and assert that their output matches that of Python's built-in
+        sort.
+        """
+        for i in range(100):
+            li = [randrange(0, 1000000) for x in range(100)]
+            expected = sorted(li)
+            li = quicksort(li)
+            self.assertEqual(expected, li)
+
     def test_insertion_sort_against_merge_sort(self):
         """Sort 100 randomly generated lists of numbers with insertion
         sort and with merge sort and assert that their outputs match.
@@ -39,6 +51,27 @@ class TestSorts(unittest.TestCase):
             merge = merge_sort(merge)
             self.assertEqual(insertion, merge)
 
+    def test_insertion_sort_against_quicksort(self):
+        """Sort 100 randomly generated lists of numbers with quicksort
+        sort and with merge sort and assert that their outputs match.
+        """
+        for i in range(100):
+            insertion = [randrange(0, 1000000) for x in range(100)]
+            quick = insertion[:]
+            insertion_sort(insertion)
+            quick = quicksort(quick)
+            self.assertEqual(insertion, quick)
+
+    def test_merge_sort_against_quicksort(self):
+        """Sort 100 randomly generated lists of numbers with merge sort
+        sort and with quicksort and assert that their outputs match.
+        """
+        for i in range(100):
+            quick = [randrange(0, 1000000) for x in range(100)]
+            merge = quick[:]
+            quick = quicksort(quick)
+            merge = merge_sort(merge)
+            self.assertEqual(quick, merge)
 
 if __name__ == '__main__':
     unittest.main()
